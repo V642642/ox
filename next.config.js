07817fe66365676d0e57fef7aae25b3d0828cd50
s0067918@gmail.com <vishal@hiringplug.com>
2023-01-09 00:58:06 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
+  "images": {unoptimized: true}
 }
 
 module.exports = nextConfig
